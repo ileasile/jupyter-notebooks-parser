2d8b15bcaad7ddd665fb9d4491b24f02fcf9fc27
Ilya Muradyan <ilya.muradyan@jetbrains.com>
2021-11-29 15:50:49 +0300

    Remove unneeded plugin management block

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,3 @@
 enableFeaturePreview("VERSION_CATALOGS")
 
-pluginManagement {
-    repositories {
-        mavenCentral()
-        maven("https://plugins.gradle.org/m2/")
-    }
-}
 rootProject.name = "jupyter-notebooks-parser"
